92ce661e702dfc893471c5430c9d3c544e4a1ada
ReQ1964 <slomianykacper4@gmail.com>
2024-12-10 17:52:26 +0100

    first commit

---
+++ src/main/java/com/req1964/matedex/MatedexApplication.java
@@ -0,0 +1,13 @@
+package com.req1964.matedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/req1964/matedex/MatedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.req1964.matedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
